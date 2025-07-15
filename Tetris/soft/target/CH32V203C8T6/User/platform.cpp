e4afaaaf17bd81bc09b31f858c503b9a20cbcde2
Roman Khvatov <rkhvatov@imachinesinc.com>
2025-07-15 17:53:49 +0100

    HW update

--- Tetris/soft/target/CH32V203C8T6/User/platform.cpp
+++ Tetris/soft/target/CH32V203C8T6/User/platform.cpp
@@ -1,4 +1,6 @@
 #include "../../../common/interface.h"
+#include "../Peripheral/inc/ch32v20x.h"
+#include "../Core/core_riscv.h"
 
 Pixels pixs;
 
@@ -9,6 +11,7 @@ static uint8_t phase;
 static uint8_t cur_keys;
 static uint8_t changed_keys;
 static uint8_t debounce;
+static uint8_t active_keys;
 
 static constexpr int debounce_time = 5;
 
@@ -89,8 +92,42 @@ EXTI9_5_IRQHandler
 // SPI1 - Cols
 // SPI2 - Rows
 
-extern "C" void TIM3_IRQHandler() __attribute__((interrupt("WCH-Interrupt-fast")));
+int get_active_adc()
+{
+    if (leds_to_sample&1) return 0;
+    if (leds_to_sample&2) return 1;
+    return -1;
+}
 
+void start_adc()
+{
+
+}
+
+void select_adc_channel()
+{
+
+}
+
+void seed_rnd_value(uint32_t val)
+{
+    CRC->DATAR = val;
+    CRC->DATAR = SysTick->CNT;
+}
+
+void seed_rnd_value()
+{
+    CRC->DATAR = val;
+}
+
+uint32_t get_random()
+{
+    seed_rnd_value();
+    return CRC->DATAR;
+}
+
+extern "C" void TIM3_IRQHandler() __attribute__((interrupt("WCH-Interrupt-fast")));
+extern "C" void ADC1_2_IRQHandler() __attribute__((interrupt("WCH-Interrupt-fast")));
 
 void TIM3_IRQHandler()
 {
@@ -112,6 +149,18 @@ void TIM3_IRQHandler()
         changed_keys |= cur_keys ^ buttons;
     }
     // ADC
+    if (phase == 2 && request_led_voltages == LEDVoltageReq::Request && ((working_pixels.br2[0]&1) || (working_pixels.br2[8]&1))
+    {
+        request_led_voltages == LEDVoltageReq::InProgress;
+        led_voltage=0;
+        led_cnt=0;
+        leds_to_sample=0;
+        if (working_pixels.br2[0]&1) leds_to_sample |= 1;
+        if (working_pixels.br2[8]&1) leds_to_sample |= 2;
+        leds_voltage[0]=leds_voltage[1]=0;
+        switch_adc_channel();
+        start_adc();        
+    }
     ++phase;
     if (phase == 3)
     {
@@ -126,9 +175,66 @@ void TIM3_IRQHandler()
             if (changed_keys)
             {
                 cur_keys ^= changed_keys;
+                active_keys = (active_keys & ~changed_keys) | (cur_keys & changed_keys);
                 debounce = debounce_time;
             }
         }
     }
     GPIOA->BSHR = 1<<31;    // Reset PA15 to '0'
 }
+
+void ADC1_2_IRQHandler()
+{
+    uint16_t adc = ADC_GetConversionValue(ADC1);
+    seed_rnd_value(adc);
+    if (!led_cnt) {led_cnt=1; start_adc(); return;}
+    led_voltage += adc;
+    ++led_cnt;
+    if (led_cnt == 5)
+    {
+        led_voltage >>= 2;
+        leds_voltage[get_active_adc()] = led_voltage;
+        leds_to_sample &= leds_to_sample - 1;
+        if (leds_to_sample)
+        {
+            led_voltage=0;
+            led_cnt=0;
+            leds_to_sample=0;
+            select_adc_channel();
+        }
+        else 
+        {
+            request_led_voltages = LEDVoltageReq::Ready;
+            return;
+        }
+    }
+    start_adc();
+}
+
+uint8_t read_key()
+{
+    while(!done)
+    {
+        __WFI();
+    }
+    done = false;
+    return active_keys;
+}
+
+void clr_keys(uint8_t keys)
+{
+    __disable_irq();
+    active_keys &= ~keys;
+    __enable_irq();
+}
+
+#define H(nm) \
+extern "C" void nm() __attribute__((interrupt("WCH-Interrupt-fast"))); \
+void nm() {seed_rnd_value();}
+
+H(EXTI0_IRQHandler)
+H(EXTI1_IRQHandler)
+H(EXTI2_IRQHandler)
+H(EXTI3_IRQHandler)
+H(EXTI4_IRQHandler)
+H(EXTI9_5_IRQHandler)
