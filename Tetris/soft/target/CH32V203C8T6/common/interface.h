c173779425c2726d043e6746659481c59388c2a5
roman-khvatov <rkhvatov@imachinesinc.com>
2026-01-07 21:35:53 +0000

    Keys mapping fixed

--- Tetris/soft/target/CH32V203C8T6/common/interface.h
+++ Tetris/soft/target/CH32V203C8T6/common/interface.h
@@ -25,14 +25,14 @@ struct Pixels {
 extern Pixels pixs;
 
 enum Key {
-    K_Up =1,
-    K_Down = 2,
-    K_Left = 4,
-    K_Right = 8,
-    K_Hit = 16,
-    K_1 = 32,
-    K_2 = 64,
-    K_3 = 128
+    K_Up    = 1<<3,
+    K_Down  = 1<<1,
+    K_Left  = 1<<0,
+    K_Right = 1<<2,
+    K_Hit   = 1<<5,
+    K_1     = 1<<7,
+    K_2     = 1<<6,
+    K_3     = 1<<4
 };
 
 ////////////////////////////
