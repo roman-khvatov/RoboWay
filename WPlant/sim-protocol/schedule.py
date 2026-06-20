df93d89a8037a567fabeace568e2ef52fbf6d5e4
roman-khvatov <rkhvatov@imachinesinc.com>
2026-06-20 19:02:18 +0100

    Startted WP Protocol simulator

---
+++ WPlant/sim-protocol/schedule.py
@@ -0,0 +1,173 @@
+﻿import asyncio
+
+__all__ = ('Event', 'WaitResult', 'Client')
+
+from dataclasses import dataclass, field
+from heapq import heappush, heappop
+from enum import Enum
+from typing import *
+
+# We wrap asyncio Event to prevent raw 'await <event>.wait()' on it, because this will break our Scheduler (Tick Increment machinery)
+class Event:
+    def __init__(self):
+        self.e = asyncio.Event()
+
+    def set(self):
+        self.e.set()
+
+    def clear(self):
+        self.e.clear()
+
+    def is_set(self) -> bool:
+        return self.e.is_set()
+
+class WaitResult(Enum):
+    Timeout = 1         # Wakeup by timeout (in 'wait()' call of Scheduler)
+    Wakeup = 2          # Wakeup by external function call (by 'wakeup()' function)
+    WakeupByEvent = 3   # Wakeup by Event raise (from 'wait(event)' call of Scheduler)
+
+@dataclass
+class _THClient:
+    client: 'Client'
+    main: Coroutine
+    task: asyncio.Task = None
+    waiting_tick: Optional[int] = None
+    waiting_event: Optional[Event] = None
+    event: asyncio.Event = None
+    result: WaitResult = None
+
+@dataclass(order=True)
+class _ClientQueueItem:
+    time: int
+    clnt: Optional[_THClient]=field(compare=False, default=None)
+    cb: Optional[Callable[[], None]] = None
+    event: Optional[Event] = None
+
+class _TimingHub:
+    def __init__(self):
+        self.clients : dict[object, _THClient] = {}
+        self.current_tick = 0
+        self.awaited_count = 0
+        self.tick_limit = None
+        self.queue : list[PrioritizedItem] = []
+
+    def add_client(self, client: 'Client', main: Coroutine):
+        self.clients[client] = _THClient(client, main)
+
+    def stop(self):
+        for clnt in self.clients.values():
+            clnt.task.cancel()
+            clnt.task = None
+
+    def run(self):
+        asyncio.run(self.main())
+
+    async def main(self):
+        async with asyncio.TaskGroup() as tg:
+            for clnt in self.clients.values():
+                clnt.task = tg.create_task(clnt.main)
+
+    async def wait(self, client, my_event: asyncio.Event, ticks: Optional[int] = None, event: Optional[Event]=None) -> WaitResult:
+        self._wakeup_by_internal_events()
+        if event and event.e.is_set():
+            return WaitResult.WakeupByEvent
+        clnt = self.clients[client]
+        clnt.event = my_event
+        clnt.waiting_event = event
+        self.awaited_count += 1
+        if ticks is not None:
+            assert tick
+            limit = ticks + self.current_tick
+            clnt.waiting_tick = limit
+            heappush(self.queue, _ClientQueueItem(limit, clnt=clnt))
+        else:
+            clnt.waiting_tick = None
+        if self.awaited_count >= len(self.clients):
+            self._advance_ticks()
+        await my_event
+        my_event.clear()
+        return clnt.result
+
+    def call_after(self, ticks: int, cb: Callable[[], None]):
+        heappush(self.queue, _ClientQueueItem(ticks + self.current_tick, cb=cb))
+
+    def fire_after(self, ticks: int, event: Event):
+        heappush(self.queue, _ClientQueueItem(ticks + self.current_tick, event=event))
+
+    def wakeup(self, client):
+        clnt = self.clients[client]
+        if not clnt.event.is_set():
+            clnt.result = WaitResult.Wakeup
+            self.awaited_count -= 1
+            clnt.event.set()
+            if clnt.waiting_tick is not None:
+                for item in self.queue:
+                    if item.clnt == clnt:
+                        item.clnt = None
+                        break
+
+    def _wakeup_by_internal_events(self):
+        for clnt in self.clients.values():
+            if clnt.waiting_event and clnt.waiting_event.e.is_set():
+                clnt.waiting_event.e.clear()
+                self.wakeup(clnt.client)
+                clnt.result = WaitResult.WakeupByEvent
+
+    def _advance_ticks(self):
+        self._wakeup_by_internal_events()
+
+        assert self.queue
+        self.current_tick = self.queue[0].time
+        while self.queue and self.queue[0].time == self.current_tick:
+            item = heappop(self.queue)
+            if item.clnt:
+                self.awaited_count -= 1
+                item.clnt.result = WaitResult.Timeout
+                item.clnt.event.set()
+            if item.cb:
+                item.cb()
+            if item.event:
+                item.event.e.set()
+        if self.tick_limit is not None and self.current_tick >= self.tick_limit:
+            self.stop()
+
+class Client:
+    root = _TimingHub()
+
+    def __init__(self, clnt_name: str, main_entry: Coroutine):
+        self.name = clnt_name
+        self._event = asyncio.Event()
+        self.root.add_client(self, main_entry)
+        self.tick_adjust = 1 # Extract from config by 'clnt_name'
+
+    async def wait(self, ticks: Optional[int] = None, event: Optional[Event]=None) -> WaitResult:
+        " Wait until 'ticks' past, or when 'wakeup()' called "
+        return await self.root.wait(self, self._event, ticks=ticks*self.tick_adjust, event=event)
+
+    @classmethod
+    def call_after(cls, ticks: int, cb: Callable[[], None]):
+        clr.root.call_after(ticks*self.tick_adjust, cb)
+
+    @classmethod
+    def fire_after(cls, ticks: int, event: Event):
+        clr.root.fire_after(ticks*self.tick_adjust, event)
+
+    def wakeup(self):
+        self.root.wakeup(self)
+
+    @classmethod
+    def current_tick(cls) -> int:
+        return cls.root.current_tick/self.tick_adjust
+
+    @classmethod
+    def set_tick_limit(cls, maximum_tick: int):
+        # Move to Config!
+        cls.root.tick_limit = maximum_tick
+
+    @classmethod
+    def run(cls):
+        cls.root.run()
+
+    @classmethod
+    def stop(cls):
+        cls.root.stop()
---
+++ WPlant/sim-protocol/uart.py
@@ -0,0 +1,66 @@
+﻿from typing import *
+
+from schedule import *
+
+ERROR = object()
+
+class UART:
+    def __init__(self, scheduler: Client, uart_name: str):
+        self.scheduler = scheduler
+        # TODO!: Load setup from Config
+        self.name = uart_name
+        self.peer = None
+        self.dir_sending = False
+        self.sender_ready = Event()
+        self.sender_ready.set()
+        self.reader_ready = Event()
+
+        self.data_rcvd : Optional[int|ERROR]= None
+        self.when_send_rdy : Optional[int] = None
+
+    def connect_to(self, peer_uart: Self):
+        assert not self.peer
+        assert not peer_uart.peer
+        self.peer = peer_uart
+        peer_uart.peer = self
+
+    async def send(self, data: int, stop_bits: int):
+        if not self.dir_sending:
+            report_error(f"UART {self.name}: Can't send - UART is read mode")
+        await self.wait_send_end()
+        self.send_now(data, stop_bits)
+
+    def send_now(self, data: int, stop_bits: int):
+        if not self.dir_sending:
+            report_error(f"UART {self.name}: Can't send - UART is read mode")
+        if not self.sender_ready.is_set():
+            report_error(f"UART {self.name}: Can't send - UART busy")
+        self.sender_ready.clear()
+        self.scheduler.fire_after(10+stop_bits, self.sender_ready)
+        if self.peer:
+            p = self.peer
+            if p.reader_ready.is_set():
+                report_warning(f"UART {self.peer.name} recieve overflow")
+            p.reader_ready.clear()
+            p.data_rcvd = data # TODO! Add here error injection and bytes lost
+            self.scheduler.fire_after(11, p.reader_ready)
+
+    async def wait_send_end(self):
+        await self.scheduler.wait(event=self.sender_ready)
+
+    async def recv(self, timeout: Optional[int]=None) -> Union[int,ERROR,None]:
+        if self.dir_sending:
+            report_error(f"UART {self.name}: Can't read - UART is send mode")
+        result = await self.scheduler.wait(ticks=timeout, event=self.reader_ready)
+        if result == WaitResult.Timeout:
+            return None
+        self.reader_ready.clear()      
+        result = self.data_rcvd
+        self.data_rcvd = None
+        return result
+
+    def set_directon(self, is_send: bool):
+        " Set direction and check for direction of own UART and connected to up/down "
+        self.dir_sending = is_send
+        if self.peer and self.dir_sending and self.peer.dir_sending:
+            report_error(f'UART {self.name} <-> {self.peer.name} write/write direction conflict')
