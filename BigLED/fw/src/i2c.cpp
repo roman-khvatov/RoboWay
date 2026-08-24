ca2469dfb33ebae44bc00685d4909e67db5295f4
Roman Khvatov <rkhvatov@imachinesinc.com>
2026-08-24 20:03:54 +0100

    FW update

---
+++ BigLED/fw/src/i2c.cpp
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+bool I2CAcceptedAddress()
+{
+    
+}
+
+bool isI2CRead()
+{
+    
+}
+
+
+bool I2CWriteReady()
+{
+    
+}
+
+bool I2CReadReady()
+{
+    
+}
+
+bool I2CAborted()
+{
+    
+}
+
+uint8_t I2CReadData()
+{
+    
+}
+
+void I2CWriteData(uint8_t data)
+{
+
+}
